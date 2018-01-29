a7b8b6b468f7b2fdc017b6d804bfaa1ac3c19708
liang <zhouliang@baiqishi.com>
2018-01-29 17:29:02 +0800

    restful jar包更改

--- check-service/src/test/java/com/zl/service/CaseTest.java
+++ check-service/src/test/java/com/zl/service/CaseTest.java
@@ -7,7 +7,7 @@ public class CaseTest {
 
     public static void main(String[] args){
         try{
-            String url = "http://127.0.0.1:21191/case/getCase";
+            String url = "http://127.0.0.1:20880/case/getCase";
             String params = "new Test";
             String result = PostUtil.sendPost(url, params);
             System.out.println(result);
