a46c0fb06d43bbd84b9d1845c014faa95156f506
liang <zhouliang@baiqishi.com>
2018-02-02 11:33:33 +0800

    learning 模块修改

--- check-learning/src/main/java/thread/ThreadWithPool.java
+++ check-learning/src/main/java/thread/ThreadWithPool.java
@@ -4,9 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import commonbeans.People;
 import threadpool.ThreadPoolUtil;
 
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 /**
  * @author zhouliang
@@ -26,6 +24,8 @@ public class ThreadWithPool {
 
     public static void main(String[] args){
 
+        CountDownLatch countDownLatch = new CountDownLatch(4);
+
         People people = new People();
         people.setName("zhouliang");
         people.setAge(21);
@@ -43,7 +43,16 @@ public class ThreadWithPool {
         threadPoolExecutor.submit(()-> System.out.println(JSONObject.toJSONString(people)));
         threadPoolExecutor.submit(()-> System.out.println(JSONObject.toJSONString(people1)));
         //用future获取值
-        Future<Integer> future = threadPoolExecutor.submit(() -> cal(12, 34));
+        Future<Integer> future = threadPoolExecutor.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                int c = cal(12, 34);
+                countDownLatch.countDown();
+                return c;
+            }
+        });
+
+        Future<Integer> another = threadPoolExecutor.submit(() -> {int c=cal(2,3);countDownLatch.countDown();return c;});
         //用futureTask获取值
         FutureTask<Integer> futureTask = new FutureTask<>(() -> cal(1, 34));
         threadPoolExecutor.submit(futureTask);
