162aa4f255c1f74a618dbeb2a25b051185cd0d4d
liang <zhouliang@baiqishi.com>
2018-05-04 09:34:21 +0800

    learning 模块修改

---
+++ check-learning/src/main/java/time/DateAndCalendarTest.java
@@ -0,0 +1,41 @@
+package time;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author zhouliang
+ * @since 2018-05-03 19:46
+ **/
+public class DateAndCalendarTest {
+    public static void main(String[] args) {
+        Date date = new Date();
+        Date start = getStartDate(date);
+        Date end = getEndDate(date);
+        System.out.println(date);
+        System.out.println(start);
+        System.out.println(end);
+    }
+
+    static Date getStartDate(Date date){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
+    }
+
+    static Date getEndDate(Date date){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+        return calendar.getTime();
+    }
+}
