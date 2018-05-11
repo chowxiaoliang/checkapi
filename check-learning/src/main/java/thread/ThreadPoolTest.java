f7b5aa49b00f9f9b1b48fc004de313f8807bf66f
liang <zhouliang@baiqishi.com>
2018-05-11 11:26:05 +0800

    learning 模块修改

--- check-learning/src/main/java/thread/ThreadPoolTest.java
+++ check-learning/src/main/java/thread/ThreadPoolTest.java
@@ -15,7 +15,7 @@ public class ThreadPoolTest {
     private static class NumberWraper{
         private volatile int value = 0;
     }
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         ThreadPoolExecutor threadPoolExecutor = ThreadPoolUtil.newIOPool("test-pool");
         NumberWraper numberWraper = new NumberWraper();
         for(int i=0;i<20000;i++){
@@ -24,6 +24,11 @@ public class ThreadPoolTest {
                     numberWraper.value++;
                 }
                 logger.info("thread {} executing 第{}次", Thread.currentThread().getName(), numberWraper.value);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             });
         }
         //关闭接收任务，但队列里面的任务会继续处理
@@ -36,6 +41,19 @@ public class ThreadPoolTest {
                 e.printStackTrace();
             }
         }while (threadPoolExecutor.getQueue().size()>0);
+
+        //等待线程池里面的任务全部处理完成
+        long taskCount ;
+        long taskCompletedCount;
+
+        do{
+            taskCount = threadPoolExecutor.getTaskCount();
+            taskCompletedCount = threadPoolExecutor.getCompletedTaskCount();
+            logger.info("task count is => {}", taskCount);
+            logger.info("completed task count is => {}", taskCompletedCount);
+            TimeUnit.MILLISECONDS.sleep(1);
+        }while (taskCount!=taskCompletedCount);
+
         logger.info("all of threads is finished");
     }
 }
