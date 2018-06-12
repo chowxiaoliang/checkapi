f16b87d568f8ae0c2141af7e22ce5f50190f893b
liang <zhouliang@baiqishi.com>
2018-06-12 10:03:19 +0800

    learning 模块修改

--- check-learning/src/main/java/commonbeans/Info.java
+++ check-learning/src/main/java/commonbeans/Info.java
@@ -36,4 +36,32 @@ public class Info implements Comparable<Info>{
     public int compareTo(Info o1) {
         return this.getAge().compareTo(o1.getAge());
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Info)) {
+            return false;
+        }
+
+        Info info = (Info) o;
+
+        if (!getName().equals(info.getName())) {
+            return false;
+        }
+        if (!getSex().equals(info.getSex())) {
+            return false;
+        }
+        return getAge().equals(info.getAge());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName().hashCode();
+        result = 31 * result + getSex().hashCode();
+        result = 31 * result + getAge().hashCode();
+        return result;
+    }
 }
