63ed13d48f5c187d4f4df6c5646eec5454d1f308
liang <zhouliang@baiqishi.com>
2018-06-14 10:24:16 +0800

    learning 模块修改

--- check-learning/src/main/java/commonbeans/Person.java
+++ check-learning/src/main/java/commonbeans/Person.java
@@ -9,11 +9,22 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
  * @since 2018-06-12 15:21
  * @desc hashCode和equals方法覆盖 关于commons lang的应用
  * https://www.ibm.com/developerworks/cn/education/java/j-lessismore/j-lessismore.html
+ * 如果最终实现 equals 方法，那么还需要实现 hashCode 方法（因为 equals 的契约表明，两个相等的对象必须有相同的散列码）
  * 实现equals方法必须遵从一定的契约
  * 1.具有反射性
  * 2.具有对称性
  * 3.具有传递性
  * 4.具有一致性
+ * 5.能够适当地处理 null
+ *
+ * 实现hashCode方法也必须遵从一定的契约
+ * hashCode 方法也有一个契约，但是不像 equals 的契约那样正式。然而，重要的是要理解它。和 equals 一样，结果必须一致。
+ * 对于对象 foo 和 bar，如果 foo.equals(bar) 返回 true，那么 foo 和 bar 的 hashCode 方法必须返回相同的值。
+ * 如果 foo 和 bar 不相等，则不要求返回不同的散列码。但是，Javadocs 提到，如果这些对象有不同的结果，那么通常会运行得更好一些。
+ *还需注意，和之前一样，如果没有覆盖它，hashCode 会返回一个看似随机的整数。
+ * 这是因为底层平台通常会将基对象的地址位置转换成一个整数；虽然如此，但文档中提到这并不是必需的，因此可以改变。
+ * 无论如何，如果最终覆盖 equals 方法，那么也有必要覆盖 hashCode 方法。
+ * 记住，虽然 hashCode 方法看上去是开箱即用的，但是 Joshua Bloch 的 Effective Java 花了 6 页的篇幅讨论如何适当地实现 hashCode 方法）
  **/
 public class Person {
 
