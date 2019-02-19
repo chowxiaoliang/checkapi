d5f6ca75be129ade93d4fcfbd35039de08f2aac5
055187 <zhouliang02@chinatopcredit.com>
2019-02-19 10:04:50 +0800

    加载器优化

--- check-learning/src/main/java/classloader/ClassLoader.java
+++ check-learning/src/main/java/classloader/ClassLoader.java
@@ -1,31 +1,24 @@
 package classloader;
 
-
-import util.ConfigUtil;
-
 /**
- * Created by Administrator on 2017/11/20 0020.
- */
+ * @author zhouliang
+ * @since 2018-06-28 11:38
+ * @desc Java虚拟机可以安装多个类加载器，系统默认三个主要的类加载器，每个加载器负责加载特定位置的类：
+         BootStrap，ExtClassLoader，AppClassLoader。
+        当Java虚拟机要加载一个类时，一般来说，一个加载请求由AppClassLoader发起，AppClassLoader找到该类则将之加载，
+        没有找到的话就委托给它的父加载器ExtClassLoader,ExtClassLoader找到则加载，没有则委托给它的父加载器BootStrapClassLoader，
+        BootStrapLoader找到该类则加载，没有找到则抛出一个ClassNotFoundException。
+ **/
 public class ClassLoader {
-
-    public static void main(String[] args){
-
-        String path1 = ClassLoader.class.getResource("").getFile();
-
-        String path2 = ClassLoader.class.getResource("/").getFile();
-
-        String path3 = ClassLoader.class.getCanonicalName();
-
-
-
-        System.out.println(path1);
-        System.out.println(path2);
-        System.out.println(path3);
-
-
-        String str = ConfigUtil.getValueByKey("es.client.transport.sniff");
-
-        System.out.println("result is :"+ str);
-
+    public static void main(String[] args) {
+        // 拿到一个类加载器
+        java.lang.ClassLoader classLoader = ClassLoader.class.getClassLoader();
+        // 循环依次遍历loader的父类
+        while (classLoader != null){
+            System.out.println("current loader is => " + classLoader);
+            classLoader = classLoader.getParent();
+        }
+        // 最顶层的loader
+        System.out.println("top loader is => " + classLoader);
     }
 }
