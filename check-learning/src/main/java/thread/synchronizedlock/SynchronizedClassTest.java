d711707250139730c1bae121678736ba1ab5cefc
055187 <zhouliang02@chinatopcredit.com>
2019-03-08 15:41:18 +0800

    synchronize

---
+++ check-learning/src/main/java/thread/synchronizedlock/SynchronizedClassTest.java
@@ -0,0 +1,49 @@
+package thread.synchronizedlock;
+
+import java.util.concurrent.TimeUnit;
+
+public class SynchronizedClassTest {
+    static String name = "zhouliang";
+    volatile static int num = 0;
+    public static void main(String[] args) {
+        synchronized (String.class){
+            Mythread mythread1 = new Mythread();
+            Mythread mythread2 = new Mythread();
+            Thread thread = new Thread(mythread1);
+            thread.start();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Thread thread1 = new Thread(mythread2);
+            thread1.start();
+        }
+    }
+
+    public synchronized void test(){
+        System.out.println("say something!");
+    }
+
+    public synchronized static void testTwo(){
+        System.out.println("say something!");
+    }
+
+    static class Mythread implements Runnable{
+        private final String innerStr = "";
+        @Override
+        public synchronized void run() {
+            synchronized (innerStr){
+                for(int i=0;i<10;i++){
+                    System.out.println("current thread is => " + Thread.currentThread().getName() + "," + "current num is => " + num);
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    num ++;
+                }
+            }
+        }
+    }
+}
