638f613f51af1e508e94bd70ddbeeafad744aba8
zhouliang <zhouliang@xbonline.net>
2019-08-29 20:40:30 +0800

    更新数据

--- check-learning/src/main/java/okooo/PageTest.java
+++ check-learning/src/main/java/okooo/PageTest.java
@@ -1,13 +1,130 @@
 package okooo;
 
+import com.alibaba.fastjson.JSONObject;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class PageTest {
 
-    public static void main(String[] args) {
+    private final static SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static void main(String[] args) throws ParseException {
+
         String url = "http://www.okooo.com/zucai/19111/";
         Document document = OkoooConnecUtil.postConnect(url);
-        System.out.println(document.toString());
+
+        // 最终结果
+        List<SuperAmbition> resultList = new ArrayList<>();
+
+        if(document != null){
+            Element bodyElement = document.body();
+            Elements periodElements = bodyElement.select(".qihao_infor");
+            // 期数
+            String periods = periodElements.get(0).html().split("期")[0];
+
+            Elements salesElements = bodyElement.select(".history_info");
+            Element salesElement = salesElements.get(0);
+            Elements salesSpanElements = salesElement.select("span");
+
+            Element salesSpanElementOne = salesSpanElements.get(0);
+            // 总销售额
+            String totalSales = salesSpanElementOne.getElementById("SaleInfoSale").html();
+
+            Element salesSpanElementTwo = salesSpanElements.get(1);
+            // 一等奖注数
+            String firstPrizeNum = salesSpanElementTwo.getElementById("SaleInfoFirstHitNum").html();
+            // 一等奖金额
+            String firstPrize = salesSpanElementTwo.getElementById("SaleInfoFirstWager").html();
+
+            Element salesSpanElementThree = salesSpanElements.get(2);
+            // 二等奖注数
+            String secondPrizeNum = salesSpanElementThree.getElementById("SaleInfoFirstHitNum").html();
+            // 二等奖金额
+            String secondPrize = salesSpanElementThree.getElementById("SaleInfoFirstWager").html();
+
+            Elements nextPondElements = bodyElement.select(".sqgc");
+            // 滚存到下期
+            String nextPond = nextPondElements.get(0).select("em").html();
+            nextPond = nextPond.replace(",", "");
+
+            Element tbodyElement = bodyElement.select("tbody").last();
+            Elements trElements = tbodyElement.select("tr");
+            for (Element trElement : trElements) {
+                Elements tdElements = trElement.select("td");
+                // 该赛事在本期次里面的顺序
+                String gameOrder = tdElements.get(0).html();
+
+                Element aElement = tdElements.get(1).select("a").get(0);
+                // 比赛类型
+                String gameType = aElement.html();
+                // 比赛时间
+                String gameTime = tdElements.get(2).attr("title").substring(5);
+
+                Element thirdElement = tdElements.get(3);
+                Elements aElements = thirdElement.select("a");
+                Element firstAelement = aElements.get(0);
+                Element secondAelement = aElements.get(1);
+                Element thirdAelement = aElements.get(2);
+                // 主场球队名称
+                String homeName = firstAelement.select("span").html();
+                // 客场球队名称
+                String guestName = thirdAelement.select("span").html();
+                // 主胜赔率
+                String homeOdds = firstAelement.select(".pltxt").first().html();
+                // 平赔
+                String dogfallOdds = secondAelement.select(".pltxt").first().html();
+                // 客胜赔率
+                String guestOdds = thirdAelement.select(".pltxt").first().html();
+                // 主场球队过往排名表现
+                String homeLastPerformance = firstAelement.select("i").first().html();
+                // 客场球队过往排名表现
+                String guestLastPerformance = thirdAelement.select("i").first().html();
+
+                Element sixthElement = tdElements.get(6);
+                // 比赛的最终比分
+                String gameScore = sixthElement.html();
+                int homeScore = Integer.valueOf(gameScore.split(":")[0].trim());
+                int guestScore = Integer.valueOf(gameScore.split(":")[1].trim());
+                // 比赛最终的结果
+                int gameResult = 1;
+                if (homeScore > guestScore) {
+                    gameResult = 3;
+                } else if (homeScore < guestScore) {
+                    gameResult = 0;
+                }
+                SuperAmbition superAmbition = new SuperAmbition();
+                superAmbition.setPeriods(periods);
+                superAmbition.setTotalSales(new BigDecimal(totalSales));
+                superAmbition.setFirstPrizeNum(Integer.valueOf(firstPrizeNum));
+                superAmbition.setFirstPrize(new BigDecimal(firstPrize));
+                superAmbition.setSecondPrizeNum(Integer.valueOf(secondPrizeNum));
+                superAmbition.setSecondPrize(new BigDecimal(secondPrize));
+                superAmbition.setNextPond(new BigDecimal(nextPond));
+                superAmbition.setGameOrder(Integer.valueOf(gameOrder));
+                superAmbition.setGameType(gameType);
+                superAmbition.setGameTime(SIMPLE_DATE_FORMAT.parse(gameTime));
+                superAmbition.setHomeName(homeName + "[" + homeOdds + "]");
+                superAmbition.setGuestName(guestName + "[" + guestOdds + "]");
+                superAmbition.setHomeLastPerformance(homeLastPerformance);
+                superAmbition.setGuestLastPerformance(guestLastPerformance);
+                superAmbition.setHomeOdds(new BigDecimal(homeOdds));
+                superAmbition.setGuestOdds(new BigDecimal(guestOdds));
+                superAmbition.setDogfallOdds(new BigDecimal(dogfallOdds));
+                superAmbition.setGameScore(gameScore);
+                superAmbition.setGameResult(gameResult);
+                superAmbition.setCreateTime(new Date());
+                resultList.add(superAmbition);
+            }
+        }
+        System.out.println(JSONObject.toJSONString(resultList));
     }
 
 }
---
+++ check-learning/src/main/java/okooo/SuperAmbition.java
@@ -0,0 +1,217 @@
+package okooo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class SuperAmbition {
+
+    private Long id;
+
+    private String periods;
+
+    private BigDecimal totalSales;
+
+    private int firstPrizeNum;
+
+    private BigDecimal firstPrize;
+
+    private int secondPrizeNum;
+
+    private BigDecimal secondPrize;
+
+    private BigDecimal nextPond;
+
+    private int gameOrder;
+
+    private String gameType;
+
+    private Date gameTime;
+
+    private String homeName;
+
+    private String guestName;
+
+    private String homeLastPerformance;
+
+    private String guestLastPerformance;
+
+    private BigDecimal homeOdds;
+
+    private BigDecimal dogfallOdds;
+
+    private BigDecimal guestOdds;
+
+    private int gameResult;
+
+    private String gameScore;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPeriods() {
+        return periods;
+    }
+
+    public void setPeriods(String periods) {
+        this.periods = periods;
+    }
+
+    public BigDecimal getTotalSales() {
+        return totalSales;
+    }
+
+    public void setTotalSales(BigDecimal totalSales) {
+        this.totalSales = totalSales;
+    }
+
+    public int getFirstPrizeNum() {
+        return firstPrizeNum;
+    }
+
+    public void setFirstPrizeNum(int firstPrizeNum) {
+        this.firstPrizeNum = firstPrizeNum;
+    }
+
+    public BigDecimal getFirstPrize() {
+        return firstPrize;
+    }
+
+    public void setFirstPrize(BigDecimal firstPrize) {
+        this.firstPrize = firstPrize;
+    }
+
+    public int getSecondPrizeNum() {
+        return secondPrizeNum;
+    }
+
+    public void setSecondPrizeNum(int secondPrizeNum) {
+        this.secondPrizeNum = secondPrizeNum;
+    }
+
+    public BigDecimal getSecondPrize() {
+        return secondPrize;
+    }
+
+    public void setSecondPrize(BigDecimal secondPrize) {
+        this.secondPrize = secondPrize;
+    }
+
+    public BigDecimal getNextPond() {
+        return nextPond;
+    }
+
+    public void setNextPond(BigDecimal nextPond) {
+        this.nextPond = nextPond;
+    }
+
+    public int getGameOrder() {
+        return gameOrder;
+    }
+
+    public void setGameOrder(int gameOrder) {
+        this.gameOrder = gameOrder;
+    }
+
+    public String getGameType() {
+        return gameType;
+    }
+
+    public void setGameType(String gameType) {
+        this.gameType = gameType;
+    }
+
+    public Date getGameTime() {
+        return gameTime;
+    }
+
+    public void setGameTime(Date gameTime) {
+        this.gameTime = gameTime;
+    }
+
+    public String getHomeName() {
+        return homeName;
+    }
+
+    public void setHomeName(String homeName) {
+        this.homeName = homeName;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getHomeLastPerformance() {
+        return homeLastPerformance;
+    }
+
+    public void setHomeLastPerformance(String homeLastPerformance) {
+        this.homeLastPerformance = homeLastPerformance;
+    }
+
+    public String getGuestLastPerformance() {
+        return guestLastPerformance;
+    }
+
+    public void setGuestLastPerformance(String guestLastPerformance) {
+        this.guestLastPerformance = guestLastPerformance;
+    }
+
+    public BigDecimal getHomeOdds() {
+        return homeOdds;
+    }
+
+    public void setHomeOdds(BigDecimal homeOdds) {
+        this.homeOdds = homeOdds;
+    }
+
+    public BigDecimal getDogfallOdds() {
+        return dogfallOdds;
+    }
+
+    public void setDogfallOdds(BigDecimal dogfallOdds) {
+        this.dogfallOdds = dogfallOdds;
+    }
+
+    public BigDecimal getGuestOdds() {
+        return guestOdds;
+    }
+
+    public void setGuestOdds(BigDecimal guestOdds) {
+        this.guestOdds = guestOdds;
+    }
+
+    public int getGameResult() {
+        return gameResult;
+    }
+
+    public void setGameResult(int gameResult) {
+        this.gameResult = gameResult;
+    }
+
+    public String getGameScore() {
+        return gameScore;
+    }
+
+    public void setGameScore(String gameScore) {
+        this.gameScore = gameScore;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
